96423406ccb2e33e1057d18a1d594e13f1b19bf3
qiaoshoutang <985622405@qq.com>
2020-06-09 19:40:47 +0800

    0609

--- themes/mobileNew/js/common.js
+++ themes/mobileNew/js/common.js
@@ -1,12 +1,28 @@
+
 s=0;
-$(".navigationBtn").on("touchstart",function () {
-   
-    if(s==0){
-        $(this).find($("#Dhl")).slideDown(500);
-        s=1;
-    }else {
-        $(this).find($("#Dhl")).slideUp(500);
-        s=0;
-    }
+y=0;
+$(document).ready(function(){
+    $(".navigationBtn").on("click",function () {
+        if(s==0){
+            $(this).next($("#Dhl")).slideDown(500);
+            s=1;
+        }else {
+            $(this).next($("#Dhl")).slideUp(500);
+            s=0;
+        }
+    });
+
+
+    $("#Loginsuccess").click(function () {
+        if(s==0){
+            $(this).next(".Login-successful").show()
+            s=1;
+        }else {
+            $(this).next(".Login-successful").hide()
+            s=0;
+        }
+
+    })
 });
+
 
